9f3442adc2f31e808cb7bf5f2be3598c0b58e211
carls1720 <quintero.carlos.rocha@gmail.com>
2023-11-09 00:00:54 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeadComponent } from './head/head.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeadComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
